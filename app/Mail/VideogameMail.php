9a97380c610948245c47ba58cebe092de1642f03
Emmy4526 <96454179+Emmy4526@users.noreply.github.com>
2023-07-25 02:50:10 -0600

    Add files via upload

---
+++ app/Mail/VideogameMail.php
@@ -0,0 +1,34 @@
+<!--
+
+// namespace App\Mail;
+
+// use Illuminate\Bus\Queueable;
+// use Illuminate\Contracts\Queue\ShouldQueue;
+// use Illuminate\Mail\Mailable;
+// use Illuminate\Mail\Mailables\Content;
+// use Illuminate\Mail\Mailables\Envelope;
+// use Illuminate\Queue\SerializesModels;
+
+// class VideogameMail extends Mailable
+// {
+//     use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+
+    */
+//     public function build()
+//     {
+//         $nombre = "Emyli ";
+//         return $this->view('mails.videogame', ['nombre'=>$nombre]);
+//     }
+// } -->
